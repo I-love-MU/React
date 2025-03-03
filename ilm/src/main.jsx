ebb3f4a58e4fb473e43331b81c5a7ff3f6d6770d
ssangttuce <sanghoo1023@gmail.com>
2025-03-03 23:06:18 +0900

    router initializing

--- ilm/src/main.jsx
+++ ilm/src/main.jsx
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import AppRouter from './route/Router' // `export default`로 가져옴
@@ -7,3 +8,9 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <AppRouter /> {/* 라우터 추가 */}
   </React.StrictMode>,
 )
+=======
+import { createRoot } from 'react-dom/client'
+import router from './route/Router'
+
+createRoot(document.getElementById('root')).render(<RouterProvider router={router} />)
+>>>>>>> f862af07 (router initializing)
--- ilm/src/route/Router.jsx
+++ ilm/src/route/Router.jsx
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 // src/route/AppRouter.jsx
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import HomePage from '../pages/HomePage'
