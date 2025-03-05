3dda4ce7f8c28683c4eee598ce38715e83561724
streqmin <sanghoo1023@gmail.com>
2025-03-05 16:11:07 +0900

    chore: 검색 페이지 라우터 추가

--- ilm/src/route/Router.jsx
+++ ilm/src/route/Router.jsx
@@ -1,11 +1,18 @@
 import { createBrowserRouter } from 'react-router-dom'
 import App from '../App'
+import SearchPage from '../pages/SearchPage'
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <App />,
-    children: [],
+    title: 'I love MU',
+    children: [
+      {
+        path: 'search',
+        element: <SearchPage />,
+      },
+    ],
   },
 ])
 
