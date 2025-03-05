0e9845bfe5783bcf9e7ed0714ae94d29bd69c7d6
streqmin <sanghoo1023@gmail.com>
2025-03-05 18:08:27 +0900

    feat: 현재 위치 조회를 체크박스 체크 후 버튼 클릭으로 실행되도록 변경

--- ilm/src/pages/SearchPage.jsx
+++ ilm/src/pages/SearchPage.jsx
@@ -1,13 +1,18 @@
 import { useState } from 'react'
 import { getLocation } from '../services/Geolocation'
 import { getAddressFromCoordinates } from '../services/GoogleMapsAddress'
+import InputGroup from 'react-bootstrap/InputGroup'
+import Form from 'react-bootstrap/Form'
+import Button from 'react-bootstrap/Button'
 
 function LocationFetcher() {
   const [location, setLocation] = useState(null)
   const [address, setAddress] = useState(null)
   const [loading, setLoading] = useState(false)
+  const [isChecked, setIsChecked] = useState(false)
 
   const fetchLocationAndAddress = async () => {
+    if (!isChecked) return
     setLoading(true)
     try {
       const loc = await getLocation()
@@ -37,9 +42,16 @@ function LocationFetcher() {
       <h2>현재 위치 주소</h2>
       <p>{address ? address : '주소 정보 없음'}</p>
 
-      <button onClick={fetchLocationAndAddress} disabled={loading}>
-        {loading ? '위치 가져오는 중...' : '위치 정보 가져오기'}
-      </button>
+      <InputGroup className='mb-3'>
+        <InputGroup.Checkbox
+          aria-label='Checkbox for following text input'
+          checked={isChecked}
+          onChange={(e) => setIsChecked(e.target.checked)}
+        />
+        <Form.Label className='ms-2'>현재 위치 탐색</Form.Label>
+      </InputGroup>
+
+      <Button onClick={fetchLocationAndAddress}>{loading ? '위치 가져오는 중...' : '위치 정보 가져오기'}</Button>
     </div>
   )
 }
