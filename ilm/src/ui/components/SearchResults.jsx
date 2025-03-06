950fa54240e86f42de6140fa1c83f62bde143e8b
9hakboiii <gyuhakarmy@gmail.com>
2025-03-06 21:06:56 +0900

    feat: 검색 결과 ui 컴포넌트 추가

---
+++ ilm/src/ui/components/SearchResults.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const SearchResults = ({ filteredData }) => {
+  return (
+    <div>
+      {filteredData && filteredData.length > 0 ? (
+        filteredData.map((data, index) => (
+          <div key={index} className="mb-3">
+            <h3>{data.title}</h3>
+            <p>장소: {data.place}</p>
+            <p>기간: {data.startDate} ~ {data.endDate}</p>
+          </div>
+        ))
+      ) : (
+        <p>검색 결과가 없습니다.</p>
+      )}
+    </div>
+  );
+};
+
+export default SearchResults;
