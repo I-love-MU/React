a0ba416452ea076d2cdb90ea6c22c3466f5cea01
youngjeaMo <tony9595@naver.com>
2025-03-07 15:35:52 +0900

    fix : 파라미터 데이터 useLocation으로 처리

--- ilm/src/pages/DetailPage.jsx
+++ ilm/src/pages/DetailPage.jsx
@@ -2,8 +2,21 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import xml2js from 'xml-js';
 
-const DetailPage = (contentIdnum = 282960) => {
+import '../css/detailcss.css';
+import { useLocation, useNavigate } from 'react-router-dom';
+
+const DetailPage = () => {
+    // 공연 상세 데이터
     const [data, setData] = useState(null);
+    
+    // 뒤로가기 버튼
+    const navigate = useNavigate();
+
+
+    // 넘어오는 sep 데이터 처리.
+    const location = useLocation();
+    const {contentNum} = location.state || {};
+    console.log(contentNum)
 
     // api key
     const apiKey = 'GdRq2wNf3V%2B2XaFCdCiLBhB7feMP3tA7no5CvoII9Mo2QkabR0cQCFR5fGsCxsphvnXhAhy8XnLX1Ep9EYT4HQ%3D%3D';
@@ -11,7 +24,7 @@ const DetailPage = (contentIdnum = 282960) => {
     // api 데이터 호출.
     useEffect(() => {
         axios
-            .get(`https://apis.data.go.kr/B553457/nopenapi/rest/publicperformancedisplays/detail?serviceKey=${apiKey}&seq=${contentIdnum}`, {
+            .get(`https://apis.data.go.kr/B553457/nopenapi/rest/publicperformancedisplays/detail?serviceKey=${apiKey}&seq=${contentNum}`, {
                 responseType: 'text'
             })
             .then((response) => {
@@ -29,15 +42,28 @@ const DetailPage = (contentIdnum = 282960) => {
             });
     }, []);
 
+    // 공연 상세데이터가 넘어오지 않을경우 처리.
     if (!data) return <div>Loading...</div>;
 
+    // 예매 사이트 url이 없을경우(무료 공연) 장소url 로 대체
     function getUrl(data) {
       return data.url && typeof data.url === "string" ? data.url : data.placeUrl;
-  }
+    }
 
+    // 공연의 내용이 없을경우 대신할 text
     function getContents(data) {
         return data.contents1 && typeof data.contents1 === "string" ? data.contents1 : "해당 공연의 정보가 없습니다.";
     }
+
+    // 날짜 데이터 변환 (YYYYMMDD) => (YYYY.MM.DD)
+    const datDate = (date) =>{
+      return `${date.substring(0,4)}.${date.substring(4,6)}.${date.substring(6,8)}`;
+    }
+
+    const back = () =>{
+      navigate(-1);
+    }
+
 
     return (
 <div id="main">
@@ -48,6 +74,8 @@ const DetailPage = (contentIdnum = 282960) => {
 
     
     <section id="mainContent">
+
+      <span onClick={back}>뒤로가기</span>
         <h3 id="contentTitle">{data.title}</h3>
         <div class="contentWrapper">
             
@@ -57,13 +85,12 @@ const DetailPage = (contentIdnum = 282960) => {
              
             <div id="contentDetail">
                 <h4>상세정보</h4>
-                <p><strong>기간 |</strong>  {data.startDate} - {data.endDate}</p>
-                <p><strong>장소 |</strong>  {data.place}/{data.area}</p>
-                <p><strong>주소 |</strong>  {data.placeAddr}</p>
-                <p><strong>관람료 |</strong>  {data.price}</p>
-                <p><strong>전화번호 |</strong>  {data.phone}</p>
-                <p><strong>사이트 |</strong>  <a href={getUrl(data)}>홈페이지 바로가기</a></p>
-                <p><a href={data.placeUrl}>이동</a></p>
+                <p><strong>기간 </strong>  {datDate(data.startDate)} ~ {datDate(data.endDate)}</p>
+                <p><strong>장소 </strong>  {data.place}/{data.area}</p>
+                <p><strong>주소 </strong>  {data.placeAddr}</p>
+                <p><strong>관람료 </strong>  {data.price}</p>
+                <p><strong>전화번호 </strong>  {data.phone}</p>
+                <p><strong>사이트 </strong>  <a href={getUrl(data)} target="_blank">홈페이지 바로가기</a></p>
             </div>
         </div>
     </section>
