e3a0be7072bfeb73548f4b525283fb71aceeef47
jieunq <63046634+jieunq@users.noreply.github.com>
2025-03-07 14:44:10 +0900

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+// 📌 src/App.jsx
+import React from "react";
+import MainPage from "./pages/MainPage";
+import "bootstrap/dist/css/bootstrap.min.css"; // 🎨 Bootstrap 스타일 적용
+
+const App = () => {
+  return <MainPage />;
+};
+
+export default App;
---
+++ src/components/CustomCarousel.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const CustomCarousel = () => {
+  return (
+    <div>
+      CustomCarousel
+    </div>
+  )
+}
+
+export default CustomCarousel
