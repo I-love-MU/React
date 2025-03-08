ed52f60299b4af8ea052bb49b5ea55bb17ef54e2
9hakboiii <gyuhakarmy@gmail.com>
2025-03-08 17:56:14 +0900

    chore: context provider 및 context import 삭제

--- ilm/src/route/Router.jsx
+++ ilm/src/route/Router.jsx
@@ -1,29 +1,22 @@
-import { createBrowserRouter } from "react-router-dom"
-import MainLayout from "../ui/layouts/MainLayout"
-import SearchPage from "../ui/pages/SearchPage"
-import { JsonDataProvider } from "../ui/contexts/JsonDataContext"
-
-
+import { createBrowserRouter } from 'react-router-dom'
+import MainLayout from '../ui/layouts/MainLayout'
+import SearchPage from '../ui/pages/SearchPage'
 
 const routes = [
-    {
-        path: '/',
-        element: <MainLayout/>,
-        loader: () => 'I love mu',
-        children: [
-            {
-                path:'search',
-                element:( 
-                <JsonDataProvider>
-                    <SearchPage/>
-                </JsonDataProvider>    
-            ),
-                loader: () => 'SEARCH'
-            },
-        ]
-    },
-]
+  {
+    path: '/',
+    element: <MainLayout />,
+    loader: () => 'I love mu',
+    children: [
+      {
+        path: 'search',
+        element: <SearchPage />,
+        loader: () => 'SEARCH'
+      },
+    ]
+  },
+];
 
 const router = createBrowserRouter(routes)
 
-export {router, routes}
+export { router, routes }
