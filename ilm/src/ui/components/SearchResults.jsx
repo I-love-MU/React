357c582f8d71019f3819db52dad59a5ae37b96fa
9hakboiii <gyuhakarmy@gmail.com>
2025-03-08 18:00:45 +0900

    refact: 검색 결과 반환 기능 별도의 컴포넌트로 분리

--- ilm/src/ui/components/SearchResults.jsx
+++ ilm/src/ui/components/SearchResults.jsx
@@ -1,21 +1,23 @@
-import React from 'react';
+import React from 'react'
 
 const SearchResults = ({ filteredData }) => {
   return (
     <div>
-      {filteredData && filteredData.length > 0 ? (
-        filteredData.map((data, index) => (
-          <div key={index} className="mb-3">
-            <h3>{data.title}</h3>
-            <p>장소: {data.place}</p>
-            <p>기간: {data.startDate} ~ {data.endDate}</p>
-          </div>
-        ))
-      ) : (
-        <p>검색 결과가 없습니다.</p>
-      )}
+        {filteredData && filteredData.length > 0 ? (
+            filteredData.map((data, index) => (
+                <div key={index} className='mb-3'>
+                    <h3>{data.title}</h3>
+                    <p>장소: {data.place}</p>
+                    <p>기간: {data.startDate} ~ {data.endDate}</p>
+                    <p>분류이름: {data.realmName}</p>
+                    <p>분류목록: {data.serviceName}</p>
+                </div>
+            ))
+        ) : (
+          <p>검색 결과가 없습니다.</p>
+        )}
     </div>
-  );
-};
+  )
+}
 
-export default SearchResults;
+export default SearchResults
