ac6cb7330c7fe99296c42046abdb4299a1e05b11
streqmin <sanghoo1023@gmail.com>
2025-03-09 17:30:14 +0900

    feat: 현재 위치 조회 코드를 임포트해서 사용하도록 수정

--- ilm/src/pages/SearchPage.jsx
+++ ilm/src/pages/SearchPage.jsx
@@ -1,50 +1,20 @@
-import { useState, useCallback, useEffect } from 'react'
-import { getLocation } from '../services/currentLocation/Geolocation'
-import { getAddressFromCoordinates } from '../services/currentLocation/GoogleMapsAddress'
+import { useRef, useState } from 'react'
 import { InputGroup, Form } from 'react-bootstrap'
 import GoogleAddressAutocomplete from '../services/pointLocation/GoogleAddressAutocomplete'
+import CurrentLocationInfo from '../services/currentLocation/CurrentLocationInfo'
 
 function LocationFetcher() {
-  const [selectedLocation, setSelectedLocation] = useState('')
-  const [coordinates, setCoordinates] = useState({ latitude: 0, longitude: 0 })
-
-  const handleLocationSelect = (location, latLng) => {
-    setSelectedLocation(location)
-    setCoordinates(latLng)
-  }
-
   // 필터 상태 관리 (라디오 버튼으로 변경)
   const [locationFilter, setLocationFilter] = useState('')
-
-  // 위치 정보 및 주소 상태 관리
-  const [currentLocation, setCurrentLocation] = useState(null)
-  const [currentAddress, setCurrentAddress] = useState(null)
-
-  // 라디오 버튼 상태 변경 핸들러
   const handleLocationFilterChange = (event) => {
     setLocationFilter(event.target.value)
   }
 
-  // 위치 정보 및 주소 가져오기
-  const fetchCurrentLocationAndAddress = useCallback(async () => {
-    try {
-      const loc = await getLocation()
-      if (loc) {
-        setCurrentLocation(loc)
-        const fetchedAddress = await getAddressFromCoordinates(loc.latitude, loc.longitude)
-        setCurrentAddress(fetchedAddress)
-      }
-    } catch (error) {
-      console.error('위치 정보를 가져오는 중 오류 발생:', error)
-    }
-  }, [])
-
-  // 라디오 버튼 값이 'current'일 때 위치 조회
-  useEffect(() => {
-    if (locationFilter === 'current') {
-      fetchCurrentLocationAndAddress()
-    }
-  }, [locationFilter, fetchCurrentLocationAndAddress])
+  // GoogleAddressAutocomplete 에서 전달받은 주소(location)를 위경도로 변환
+  const coordinates = useRef({ latitude: 126.734086, longitude: 127.269311 })
+  const handleLocationSelect = (location) => {
+    console.log(location, coordinates)
+  }
 
   return (
     <div>
@@ -66,21 +36,8 @@ function LocationFetcher() {
           onChange={handleLocationFilterChange}
         />
       </InputGroup>
-      {locationFilter === 'current' && (
-        <>
-          <h2>현재 위치 정보</h2>
-          {currentLocation ? (
-            <p>
-              위도: {currentLocation.latitude}, 경도: {currentLocation.longitude}, 정확도: {currentLocation.accuracy}m
-            </p>
-          ) : (
-            <p>위치 정보를 가져오려면 옵션을 선택하세요.</p>
-          )}
 
-          <h2>현재 위치 주소</h2>
-          <p>{currentAddress ? currentAddress : '주소 정보 없음'}</p>
-        </>
-      )}
+      <CurrentLocationInfo locationFilter={locationFilter} />
 
       {locationFilter === 'address' && (
         <>
