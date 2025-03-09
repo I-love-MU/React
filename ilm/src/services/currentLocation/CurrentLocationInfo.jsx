0d72b4f0362c549124e5dacd46272bbc20981c48
streqmin <sanghoo1023@gmail.com>
2025-03-10 00:55:41 +0900

    feat: 검색 필터를 전달받지 않고, 렌더링 될 때마다 현재 위치를 확인하도록 수정. 위경도와 주소를 함께 검색 페이지에 전달하도록 수정

--- ilm/src/services/currentLocation/CurrentLocationInfo.jsx
+++ ilm/src/services/currentLocation/CurrentLocationInfo.jsx
@@ -1,50 +1,25 @@
-import { useState, useCallback, useEffect } from 'react'
+import { useCallback, useEffect } from 'react'
 import { getLocation } from './Geolocation'
 import { getAddressFromCoordinates } from './GoogleMapsAddress'
 
-function CurrentLocationInfo({ locationFilter, onLocationUpdate }) {
-  const [currentLocation, setCurrentLocation] = useState(null)
-  const [currentAddress, setCurrentAddress] = useState(null)
-
-  // 현재 위치 정보 및 주소 가져오기
-  const fetchCurrentLocationAndAddress = useCallback(async () => {
+function CurrentLocationInfo({ onLocationUpdate }) {
+  // 현재 위경도와 주소를 반환
+  const getCurrentLocation = useCallback(async () => {
     try {
       const loc = await getLocation()
-      if (loc) {
-        setCurrentLocation(loc)
-        onLocationUpdate({ latitude: loc.latitude, longitude: loc.longitude })
-        const fetchedAddress = await getAddressFromCoordinates(loc.latitude, loc.longitude)
-        setCurrentAddress(fetchedAddress)
-      }
+      const fetchedAddress = await getAddressFromCoordinates(loc.latitude, loc.longitude)
+      const newLocation = { latitude: loc.latitude, longitude: loc.longitude, address: fetchedAddress }
+      onLocationUpdate(newLocation)
     } catch (error) {
       console.error('위치 정보를 가져오는 중 오류 발생:', error)
     }
-  }, [onLocationUpdate])
+  }, [])
 
-  // locationFilter가 'current'일 때만 위치 조회 실행
   useEffect(() => {
-    if (locationFilter === 'current') {
-      fetchCurrentLocationAndAddress()
-    }
-  }, [locationFilter, fetchCurrentLocationAndAddress])
+    getCurrentLocation()
+  }, [])
 
-  if (locationFilter !== 'current') return null
-
-  return (
-    <>
-      <h2>현재 위치 좌표</h2>
-      {currentLocation ? (
-        <p>
-          위도: {currentLocation.latitude}, 경도: {currentLocation.longitude}, 정확도: {currentLocation.accuracy}m
-        </p>
-      ) : (
-        <p>위치 정보를 가져오지 못 했습니다.</p>
-      )}
-
-      <h2>현재 위치 주소</h2>
-      <p>{currentAddress ? currentAddress : '주소 정보 없음'}</p>
-    </>
-  )
+  return <></>
 }
 
 export default CurrentLocationInfo
