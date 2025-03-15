d802d4c08ecb226a593d0e5ca6bd73725ff50c93
ParkJaejeong <cront48311@gmail.com>
2025-03-15 21:17:22 +0900

    feat. category right

--- ilm/src/components/Header.jsx
+++ ilm/src/components/Header.jsx
@@ -5,7 +5,7 @@ const Header = () => {
   return (
     <header style={{ textAlign: 'right', padding: '20px' }}>
       <nav>
-        <ul style={{ display: 'flex', listStyle: 'none', gap: '15px', fontSize: '18px' }}>
+        <ul style={{ display: 'flex', listStyle: 'none', gap: '15px', fontSize: '18px', justifyContent: 'flex-end', }}>
           <li><a href="#theatre">THEATRE</a></li>
           <li><a href="#concert">CONCERT</a></li>
           <li><a href="#exhibition">EXHIBITION</a></li>
